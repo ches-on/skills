b3b5b611ffe4e855abbd19f8363a194a2b225943
justsus cheson <113528339+ches-on@users.noreply.github.com>
2026-05-04 14:32:58 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -64,14 +64,10 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
     )
-}
-
-
-
-
-    
+}    
     
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
